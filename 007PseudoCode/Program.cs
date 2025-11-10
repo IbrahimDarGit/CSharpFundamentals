cbd5308d515c6c1f56bffed6c3d87672cfaf9aa0
IbrahimDarGit <muhammad.ibrhim.dar@outlook.com>
2025-11-10 20:27:00 +0000

    Example of how to Convert Pseudocode into C#

---
+++ 007PseudoCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _007PseudoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example of simple pseudocode translated to C#
+
+            /*
+               ThePseudocode I was given:
+               INPUT number
+               IF number > 0 THEN
+                    OUTPUT "Positive"
+               ELSE
+                    OUTPUT "Negative or Zero"
+               ENDIF
+            */
+            // Translated C# code:
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+                Console.WriteLine("Positive");
+            else
+                Console.WriteLine("Negative or Zero");
+        }
+    }
+}
