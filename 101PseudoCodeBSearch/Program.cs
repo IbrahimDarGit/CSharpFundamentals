711c040ce6b53af7d5573716232f2d15239ef74a
IbrahimDarGit <muhammad.ibrhim.dar@outlook.com>
2025-11-17 22:08:09 +0000

    Pesudo code into C# for Trace table

---
+++ 101PseudoCodeBSearch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _101PseudoCodeBSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 3, 7, 8, 9, 27, 35, 45, 55};
+            int ItemToFind = 27;
+
+            int f = 0;
+            int l = 7;
+            int m = 0;
+            bool found = false;
+            bool failed = false;
+
+            while (!found && !failed)
+            {
+ 
+                m=(f + l) / 2;
+ Console.WriteLine("f=" + f + " , l=" + l + " , ItemToFind=" + ItemToFind + " , found=" + found + " , failed=" + failed + " , m=" + m);               
+                if (list[m] == ItemToFind)
+                {
+                    found = true;
+                }
+                else if (f >= l)
+                {
+                    failed = true;
+                }
+                else
+                {
+                    if (list[m] > ItemToFind)
+                    {
+                        l = m - 1;
+                    }
+                    else
+                    {
+                        f = m + 1;
+                    }
+                }
+           
+            }   
+
+ Console.WriteLine("f=" + f + " , l=" + l + " , ItemToFind=" + ItemToFind + " , found=" + found + " , failed=" + failed + " , m=" + m);                  
+        }
+    }
+}
---
+++ 101PseudoCodeQ6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = 6;
+        int P = 0;
+
+        Console.WriteLine("N  P  X  Y  F");
+
+        for (int X = 2; X <= N; X++)
+        {
+            int F = 0;
+
+            for (int Y = 2; Y <= 3; Y++)
+            {
+
+                Console.WriteLine(N + "  " + P + "  " + X + "  " + Y + "  " + F);
+
+                if ((X % Y == 0) && (X != Y))
+                {
+                    F = F + 1;
+                }
+            }
+
+            if (F == 0)
+            {
+                P = P + 1;
+            }
+
+
+            Console.WriteLine(N + "  " + P + "  " + X + "  -  " + F);
+        }
+    }
+}
