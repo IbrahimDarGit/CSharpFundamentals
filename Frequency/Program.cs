f869799d4c963c846c67b2dcd18a56210cb47286
IbrahimDarGit <muhammad.ibrhim.dar@outlook.com>
2026-03-01 19:24:28 +0000

    IbrahimDar Auto update Push on 2026Mar01T192428

---
+++ Frequency/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your number sequence:");
+            string seq = Console.ReadLine();
+            string[] num = seq.Split(',');
+
+            int[] frequency = new int[10];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int value = int.Parse(num[i]);
+                frequency[value] = frequency[value] + 1;
+            }
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine(i + "    " + frequency[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
