821cd8323d21c695c48c3eeed43054056bc98e74
IbrahimDarGit <muhammad.ibrhim.dar@outlook.com>
2026-03-08 21:54:10 +0000

    IbrahimDar Auto update Push on 2026Mar08T215409

---
+++ PrimeAsc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ascending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            for (int n = 2; n < 1000000; n++)
+            {
+                bool prime = true;
+
+                for (int i = 2; i * i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime)
+                {
+                    string s = n.ToString();
+                    bool ascending = true;
+
+                    for (int i = 1; i < s.Length; i++)
+                    {
+                        if (s[i] <= s[i - 1])
+                        {
+                            ascending = false;
+                            break;
+                        }
+                    }
+
+                    if (ascending)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Count: {0}", count);
+        }
+    }
+}
