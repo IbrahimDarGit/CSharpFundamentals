96a8cc48cf212c0d610457ace6a7aa48b5ffe897
IbrahimDarGit <muhammad.ibrhim.dar@outlook.com>
2026-03-15 19:06:05 +0000

    IbrahimDar Auto update Push on 2026Mar15T190605

---
+++ LetterCount/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LetterCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+            string sentence = Console.ReadLine();
+
+            int[] frequency = new int[256];
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                char c = sentence[i];
+                frequency[c]++;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                if (frequency[i] > 0)
+                {
+                    if ((char)i == ' ')
+                    {
+                        Console.WriteLine("<> - " + frequency[i]);
+                    }
+                    else
+                    {
+                        Console.WriteLine((char)i + " - " + frequency[i]);
+                    }
+                }
+            }
+        }
+    }
+}
