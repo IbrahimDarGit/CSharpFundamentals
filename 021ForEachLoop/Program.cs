04d7f164495fb9c2cee5142fe2f3789433b4deef
IbrahimDarGit <muhammad.ibrhim.dar@outlook.com>
2026-03-22 10:09:05 +0000

    IbrahimDar Auto update Push on 2026Mar22T100904

---
+++ 021ForEachLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _021ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Rowena", "Robin", "Bao" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ DoubleLetter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DoubleLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word:");
+            string input = Console.ReadLine();
+            Console.WriteLine("You entered: " + input);
+            int count = 0;
+            string doubleLetters = "";
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    count++;
+                    doubleLetters += input[i];
+                }
+            }
+            if (count > 0)
+            {
+                Console.WriteLine("The word contains " + count + " double letters: " + doubleLetters);
+            }
+            else if (count > 1)
+            {
+                Console.WriteLine("The word contains more than one double letter.");
+                Console.WriteLine("The double letters are: " + doubleLetters);
+            }
+            else
+            {
+                Console.WriteLine("The word does not contain any double letters.");
+            }
+        }
+    }
+}
